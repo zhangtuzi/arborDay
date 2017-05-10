64b08a0a34184e9afdcf6e7e2bbb921146355bf5
zhangtuzi <1243933019@qq.com>
2017-05-10 09:14:57 +0800

    '奖品详情弹层高度自适应'

--- js/new3_treasure.js
+++ js/new3_treasure.js
@@ -4,7 +4,10 @@ $(function(){
   var use_con_height=$('#use_con').height();
   var cj_fail_height=$('#cj_fail').height();
   $('.mc,.tc_div,.warpAll').height(win_height);
-  $('#use_instruction').height(win_height*0.74);
+  $('#use_instruction').css('max-height',win_height*0.74);
+  var use_instruction_height=$('#use_instruction').height();
+   $('.use_rule_word').css('max-height',win_height*0.74-80);
+  $('#use_instruction').css('top',(win_height-use_instruction_height)/2);
   $('#use_con').css('top',(win_height-use_con_height)/2);
   $('#cj_fail').css('top',(win_height-cj_fail_height)/2);
 })
